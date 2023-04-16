fcada4c6d6b27cd7efeece9ef434ba6d42973457
Greg Kreisman <gkreisman_cep@qq.com>
2023-04-16 22:01:41 +0800

    Add files via upload

---
+++ tapcodearrayled3NoPiezo/tapcodearrayled3NoPiezo.ino
@@ -0,0 +1,78 @@
+// C++ code
+//
+#include <Servo.h>
+
+Servo myservo;
+ #define servoPin 9
+
+int angle =0;
+// use EF92A 180d analog micro servo. not the EF90D 360d digital servo, as this one keeps spinning during delay()
+// https://github.com/greggelong/arduinoTapCode
+
+// the Piezo seems to be messing with the servo so I am taking it out
+// translate sentence with python code to save arduino memory
+
+
+int s[] = {0,22,42,15,22,0,24,43,0,22,34,34,14};// 
+void setup()
+{
+  myservo.attach(servoPin);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  //pinMode(3,OUTPUT);
+  
+  
+}
+
+void loop()
+{
+  for (int j = 0; j<13;j++){
+    if (s[j]==0){
+      // go backward for start of word
+      // move servo
+      myservo.write(0);
+      // light green leg
+      digitalWrite(7,HIGH);
+      // piezo
+      //analogWrite(3,20);
+  	  delay(2000);
+      myservo.write(0);
+      digitalWrite(7,LOW);
+      //analogWrite(3,0);
+      delay(4000);
+      
+    }
+    else{
+      // start tapping out letter in word
+      // use math to get the number in 10s place
+        int lttr = s[j];
+  		for (int i = 0; i<floor(lttr/10);i++){
+  		myservo.write(70);
+        digitalWrite(8,HIGH);
+        // piezo
+        //analogWrite(3,20);
+  		delay(1000);
+  		myservo.write(0);
+        digitalWrite(8,LOW);
+        //analogWrite(3,0);
+  		delay(1000);
+ 		 }
+  		// delay between letter half
+        // use math to get the number in the 1s place
+  		delay(2000);
+  		for (int i =0; i <lttr%10;i++){
+  		myservo.write(70);
+        digitalWrite(8,HIGH);
+        // piezo
+        //analogWrite(3,20);
+  		delay(1000);
+  		myservo.write(0);
+        digitalWrite(8,LOW);
+        //analogWrite(3,0);
+  		delay(1000);
+  		}
+  		// delay between letters
+  		delay(4000);
+   		 }
+  }
+}
